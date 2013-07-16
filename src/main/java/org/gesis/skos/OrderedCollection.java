3c267d989e94ddc487d39ec39988a0ad9ea907ca
Matthäus Zloch <matthaeus.zloch@gmail.com>
2013-07-16 14:24:23 +0200

    implements IPersistableResource

--- src/main/java/org/gesis/skos/OrderedCollection.java
+++ src/main/java/org/gesis/skos/OrderedCollection.java
@@ -3,18 +3,22 @@ package org.gesis.skos;
 import java.util.LinkedHashSet;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.OneToOne;
 
+import org.gesis.ddi.util.IdentifierFactory;
+import org.gesis.persistence.IPersistableResource;
 import org.gesis.rdf.List;
 import org.gesis.rdfs.Resource;
 
 @Entity
 @Inheritance( strategy = InheritanceType.JOINED )
-public class OrderedCollection extends LinkedHashSet<Resource> implements Collection
+public class OrderedCollection extends LinkedHashSet<Resource> implements Collection, IPersistableResource
 {
 
 	/**
@@ -24,12 +28,24 @@ public class OrderedCollection extends LinkedHashSet<Resource> implements Collec
 
 	// properties
 
+	@Column
+	@Id
+	private String id;
+
+	@Column
+	private String urn;
+
 	// relations
 
 	@OneToOne( cascade = CascadeType.ALL, fetch = FetchType.LAZY )
 	private List skos_memberList;
 
 	// getter / setter
+
+	public OrderedCollection()
+	{
+		id = IdentifierFactory.getNextDefaultIdentifier();
+	}
 
 	public List getSkos_memberList()
 	{
@@ -41,5 +57,26 @@ public class OrderedCollection extends LinkedHashSet<Resource> implements Collec
 		this.skos_memberList = skos_memberList;
 	}
 
+	@Override
+	public String getId()
+	{
+		return id;
+	}
+
+	public void setId( final String id )
+	{
+		this.id = id;
+	}
+
+	@Override
+	public String getURN()
+	{
+		return urn;
+	}
+
+	public void setUrn( final String urn )
+	{
+		this.urn = urn;
+	}
 
 }
