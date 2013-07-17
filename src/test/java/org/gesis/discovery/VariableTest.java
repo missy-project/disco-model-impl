3d9469706272630c610280a3e3889a0afcda718a
Matthäus Zloch <matthaeus.zloch@gmail.com>
2013-07-17 19:19:21 +0200

    moved to proper package

---
+++ src/test/java/org/gesis/discovery/VariableTest.java
@@ -0,0 +1,50 @@
+package org.gesis.discovery;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.gesis.discovery.Representation;
+import org.gesis.discovery.Variable;
+import org.gesis.rdfs.Datatype;
+import org.gesis.skos.ConceptScheme;
+import org.gesis.skos.OrderedCollection;
+import org.junit.Test;
+
+public class VariableTest
+{
+
+	@Test
+	public void setOrderedCollection()
+	{
+		Representation representation = new OrderedCollection();
+
+		Variable variable = new Variable();
+		variable.setRepresentation( representation );
+
+		assertNotNull( variable.getRepresentation() );
+		assertTrue( variable.getRepresentation() instanceof OrderedCollection );
+	}
+
+	@Test
+	public void setDatatype()
+	{
+		Representation representation = new Datatype();
+
+		Variable variable = new Variable();
+		variable.setRepresentation( representation );
+
+		assertNotNull( variable.getRepresentation() );
+		assertTrue( variable.getRepresentation() instanceof Datatype );
+	}
+
+	@Test
+	public void setConecptScheme()
+	{
+		Representation representation = new ConceptScheme();
+
+		Variable variable = new Variable();
+		variable.setRepresentation( representation );
+
+		assertNotNull( variable.getRepresentation() );
+		assertTrue( variable.getRepresentation() instanceof ConceptScheme );
+	}
+}
