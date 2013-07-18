2f9dea29882445f3f23d4929c1aef892937add22
Matthäus Zloch <matthaeus.zloch@gmail.com>
2013-07-18 13:06:02 +0200

    removed redundant properties and methods

--- src/main/java/org/gesis/skos/OrderedCollection.java
+++ src/main/java/org/gesis/skos/OrderedCollection.java
@@ -1,15 +1,12 @@
 package org.gesis.skos;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.OneToOne;
 
-import org.gesis.ddi.util.IdentifierFactory;
 import org.gesis.discovery.Representation;
 import org.gesis.rdf.List;
 
@@ -20,24 +17,12 @@ public class OrderedCollection extends Representation
 
 	// properties
 
-	@Column
-	@Id
-	private String id;
-
-	@Column
-	private String urn;
-
 	// relations
 
 	@OneToOne( cascade = CascadeType.ALL, fetch = FetchType.LAZY )
 	private List memberList;
 
 	// getter / setter
-
-	public OrderedCollection()
-	{
-		id = IdentifierFactory.getNextDefaultIdentifier();
-	}
 
 	public List getMemberList()
 	{
@@ -47,28 +32,6 @@ public class OrderedCollection extends Representation
 	public void setMemberList( final List memberList )
 	{
 		this.memberList = memberList;
-	}
-
-	@Override
-	public String getId()
-	{
-		return id;
-	}
-
-	public void setId( final String id )
-	{
-		this.id = id;
-	}
-
-	@Override
-	public String getURN()
-	{
-		return urn;
-	}
-
-	public void setUrn( final String urn )
-	{
-		this.urn = urn;
 	}
 
 }
