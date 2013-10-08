e66686dc1e1bc3c7ea0526c1ef455605c9ebcf58
matthaeus <matthaeus.zloch@gmail.com>
2013-10-08 11:24:26 +0200

    StudyGroup extends UnionStudyGroupStudy

--- src/main/java/org/gesis/discovery/StudyGroup.java
+++ src/main/java/org/gesis/discovery/StudyGroup.java
@@ -4,11 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 
-import org.gesis.rdfs.Resource;
-
 @Entity
 @Inheritance( strategy = InheritanceType.JOINED )
-public class StudyGroup extends Resource
+public class StudyGroup extends Union_StudyGroupStudy
 {
 
 }
