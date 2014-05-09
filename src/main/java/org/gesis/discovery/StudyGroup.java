7523af84b640b2416fd5940d608a85d6b8d9abb9
Matthaeus Zloch <matthaeus.zloch@gmail.com>
2014-05-09 16:11:38 +0200

    added getStudies -> mappedBy("inGroup") of StudyGroup

--- src/main/java/org/gesis/discovery/StudyGroup.java
+++ src/main/java/org/gesis/discovery/StudyGroup.java
@@ -1,12 +1,32 @@
 package org.gesis.discovery;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
 
 @Entity
 @Inheritance( strategy = InheritanceType.JOINED )
 public class StudyGroup extends Union_StudyGroupStudy
 {
+
+	// relations
+
+	@OneToMany( mappedBy = "inGroup" )
+	private List<Study> studies;
+
+	// getter / setter
+
+	public List<Study> getStudies()
+	{
+		return studies;
+	}
+
+	public void setStudies( final List<Study> studies )
+	{
+		this.studies = studies;
+	}
 
 }
