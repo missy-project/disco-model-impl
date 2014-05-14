00145ea4041907fee7672d2fc60db668f4f526b9
Matthaeus Zloch <matthaeus.zloch@gmail.com>
2014-05-14 17:05:36 +0200

    label of Location is a @Lob

--- src/main/java/org/gesis/dcterms/Location.java
+++ src/main/java/org/gesis/dcterms/Location.java
@@ -1,8 +1,10 @@
 package org.gesis.dcterms;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.Lob;
 
 import org.gesis.rdfs.Resource;
 
@@ -13,6 +15,8 @@ public class Location extends Resource
 
 	// properties
 
+	@Column
+	@Lob
 	private String label;
 
 	// getter/setter
