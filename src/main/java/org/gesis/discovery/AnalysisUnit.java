fb07c6931e32747e71a5b89a6155342b810640dd
Matthäus Zloch <matthaeus.zloch@gmail.com>
2018-01-30 10:33:02 +0100

    add documentation for AnalysisUnit and Universe

--- src/main/java/org/gesis/discovery/AnalysisUnit.java
+++ src/main/java/org/gesis/discovery/AnalysisUnit.java
@@ -6,6 +6,19 @@ import javax.persistence.InheritanceType;
 
 import org.gesis.skos.Concept;
 
+/**
+ * <p>
+ * An AnalysisUnit is defined as follows: The process of collecting data focuses
+ * on the analysis of a particular type of subject. If, for example, the adult
+ * population of Finland is being studied, the AnalysisUnit would be individuals
+ * or persons.
+ * </p>
+ * <p>
+ * An AnalysisUnit is a {@link Concept}.
+ * 
+ * @author matthaeus
+ * @see {@link http://rdf-vocabulary.ddialliance.org/discovery.html#analysis-units-and-universes}
+ */
 @Entity
 @Inheritance( strategy = InheritanceType.JOINED )
 public class AnalysisUnit extends Concept
@@ -16,4 +29,9 @@ public class AnalysisUnit extends Concept
 	 */
 	private static final long serialVersionUID = 1L;
 
+	// properties
+
+	// relations
+
+	// getter/setter
 }
--- src/main/java/org/gesis/discovery/Universe.java
+++ src/main/java/org/gesis/discovery/Universe.java
@@ -6,6 +6,22 @@ import javax.persistence.InheritanceType;
 
 import org.gesis.skos.Concept;
 
+/**
+ * <p>
+ * Universe is the total membership or population of a defined class of people,
+ * objects or events. There are two types of population, target population and
+ * survey population. A target population is the population outlined in the
+ * survey objects about which information is to be sought. A survey population
+ * (also known as the coverage of the survey) is the population from which
+ * information can be obtained in the survey.
+ * </p>
+ * <p>
+ * A Universe is a {@link Concept}
+ * </p>
+ * 
+ * @author matthaeus
+ * @see {@link http://rdf-vocabulary.ddialliance.org/discovery.html#analysis-units-and-universes}
+ */
 @Entity
 @Inheritance( strategy = InheritanceType.JOINED )
 public class Universe extends Concept
